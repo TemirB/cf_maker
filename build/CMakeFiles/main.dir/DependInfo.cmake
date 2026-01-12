a0074c2b43eff18a30e2b541ef136ddfee430b9b
Temir <balinov.tm21@physics.msu.ru>
2026-01-12 18:47:58 +0300

    fix(general): Delete build dir

---
+++ build/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/ubuntu/Temir/smash/build/data/cf_maker_new/main.cpp" "CMakeFiles/main.dir/main.cpp.o" "gcc" "CMakeFiles/main.dir/main.cpp.o.d"
+  "/home/ubuntu/Temir/smash/build/data/cf_maker_new/src/cf3d.cpp" "CMakeFiles/main.dir/src/cf3d.cpp.o" "gcc" "CMakeFiles/main.dir/src/cf3d.cpp.o.d"
+  "/home/ubuntu/Temir/smash/build/data/cf_maker_new/src/draw.cpp" "CMakeFiles/main.dir/src/draw.cpp.o" "gcc" "CMakeFiles/main.dir/src/draw.cpp.o.d"
+  "/home/ubuntu/Temir/smash/build/data/cf_maker_new/src/fit.cpp" "CMakeFiles/main.dir/src/fit.cpp.o" "gcc" "CMakeFiles/main.dir/src/fit.cpp.o.d"
+  "/home/ubuntu/Temir/smash/build/data/cf_maker_new/src/helpers.cpp" "CMakeFiles/main.dir/src/helpers.cpp.o" "gcc" "CMakeFiles/main.dir/src/helpers.cpp.o.d"
+  "/home/ubuntu/Temir/smash/build/data/cf_maker_new/src/plots.cpp" "CMakeFiles/main.dir/src/plots.cpp.o" "gcc" "CMakeFiles/main.dir/src/plots.cpp.o.d"
+  "/home/ubuntu/Temir/smash/build/data/cf_maker_new/src/proj1d.cpp" "CMakeFiles/main.dir/src/proj1d.cpp.o" "gcc" "CMakeFiles/main.dir/src/proj1d.cpp.o.d"
+  "/home/ubuntu/Temir/smash/build/data/cf_maker_new/src/proj2d.cpp" "CMakeFiles/main.dir/src/proj2d.cpp.o" "gcc" "CMakeFiles/main.dir/src/proj2d.cpp.o.d"
+  "/home/ubuntu/Temir/smash/build/data/cf_maker_new/src/ratios.cpp" "CMakeFiles/main.dir/src/ratios.cpp.o" "gcc" "CMakeFiles/main.dir/src/ratios.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/main.cpp.o"
+  "CMakeFiles/main.dir/main.cpp.o.d"
+  "CMakeFiles/main.dir/src/cf3d.cpp.o"
+  "CMakeFiles/main.dir/src/cf3d.cpp.o.d"
+  "CMakeFiles/main.dir/src/draw.cpp.o"
+  "CMakeFiles/main.dir/src/draw.cpp.o.d"
+  "CMakeFiles/main.dir/src/fit.cpp.o"
+  "CMakeFiles/main.dir/src/fit.cpp.o.d"
+  "CMakeFiles/main.dir/src/helpers.cpp.o"
+  "CMakeFiles/main.dir/src/helpers.cpp.o.d"
+  "CMakeFiles/main.dir/src/plots.cpp.o"
+  "CMakeFiles/main.dir/src/plots.cpp.o.d"
+  "CMakeFiles/main.dir/src/proj1d.cpp.o"
+  "CMakeFiles/main.dir/src/proj1d.cpp.o.d"
+  "CMakeFiles/main.dir/src/proj2d.cpp.o"
+  "CMakeFiles/main.dir/src/proj2d.cpp.o.d"
+  "CMakeFiles/main.dir/src/ratios.cpp.o"
+  "CMakeFiles/main.dir/src/ratios.cpp.o.d"
+  "main"
+  "main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
