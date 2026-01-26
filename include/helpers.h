0f6372f9006ec8f2b6ad0363ef5f80e9e3f42034
Temir Balinov <balinov.tm21@physics.msu.ru>
2026-01-26 20:33:35 +0300

    fix(general): cosmetic changes

--- include/helpers.h
+++ include/helpers.h
@@ -13,7 +13,7 @@ inline constexpr int rapiditySize = 10;
 inline constexpr int lcmsSize = 3;
 
 const std::array<double, 2> rapidityValues = {-1.0, 1.0};
-const std::array<double, 5>      ktValues        = {0.15, 0.25, 0.35, 0.45, 0.60};
+const std::array<double, 5> ktValues = {0.15, 0.25, 0.35, 0.45, 0.60};
 const std::array<int, 4> colors = {kRed, kBlue, kMagenta, kGreen};
 const std::array<int, 4> markers = {20, 21, 22, 23};
 
