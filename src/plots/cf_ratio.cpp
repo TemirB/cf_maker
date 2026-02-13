3487d91fd39aca949679a72994e1e14f61c9627a
Temir Balinov <balinov.tm21@physics.msu.ru>
2026-02-13 03:22:47 +0300

    fix(kt_bining): Fixed titles

--- src/plots/cf_ratio.cpp
+++ src/plots/cf_ratio.cpp
@@ -12,153 +12,6 @@
 #include "fit/types.h"
 #include "helpers.h"
 
-template<class T>
-using RootPtr = std::unique_ptr<T>;
-
-std::string GetCentralityLabel(int centIdx) {
-    static const std::vector<std::string> labels = {
-        "0-5%", "5-10%", "10-20%", "20-40%", "40-60%", "60-80%"
-    };
-    return (centIdx >= 0 && centIdx < (int)labels.size()) 
-        ? labels[centIdx] : Form("cent %d", centIdx);
-}
-
-std::string GetKTRangeLabel(int ktIdx) {
-    static const std::vector<std::string> labels = {
-        "0.1-0.2", "0.2-0.3", "0.3-0.4", "0.4-0.5", "0.5-0.6", "0.6-0.8", "0.8-1.0"
-    };
-    return (ktIdx >= 0 && ktIdx < (int)labels.size()) 
-        ? Form("k_{T} = %s GeV/c", labels[ktIdx].c_str()) 
-        : Form("k_{T} bin %d", ktIdx);
-}
-
-std::string FormatRatioName(
-    const char* prefix, 
-    int centIdx, 
-    int ktIdx, 
-    LCMSAxis axis
-) {
-    return Form("%s_%d_%d_%s", prefix, centIdx, ktIdx, ToString(axis).c_str());
-}
-
-void ApplyRatioStyle(TH1D* h, LCMSAxis axis) {
-    if (!h) return;
-    
-    // Подписи осей
-    const char* axisLabel = 
-        axis == LCMSAxis::Out  ? "q_{out} (GeV/c)" :
-        axis == LCMSAxis::Side ? "q_{side} (GeV/c)" : "q_{long} (GeV/c)";
-    
-    h->GetXaxis()->SetTitle(axisLabel);
-    h->GetYaxis()->SetTitle("CF^{-} / CF^{+}");
-    h->SetTitle("");
-    
-    // Стиль осей
-    h->GetXaxis()->SetTitleSize(0.055);
-    h->GetXaxis()->SetLabelSize(0.05);
-    h->GetXaxis()->SetTitleOffset(1.1);
-    h->GetXaxis()->CenterTitle();
-    
-    h->GetYaxis()->SetTitleSize(0.055);
-    h->GetYaxis()->SetLabelSize(0.05);
-    h->GetYaxis()->SetTitleOffset(1.0);
-    h->GetYaxis()->CenterTitle();
-    
-    // Стиль маркеров (частицы/античастицы)
-    h->SetMarkerStyle(kFullCircle);   // 20 = filled circle
-    h->SetMarkerSize(1.2);
-    h->SetMarkerColor(kBlack);
-    h->SetLineColor(kBlack);
-    h->SetLineWidth(1);
-    
-    // Адаптивный диапазон Y с защитой от выбросов
-    double ymin = h->GetMinimum(0.0);  // игнорируем бины с 0
-    double ymax = h->GetMaximum();
-    
-    if (ymin < 0.2) ymin = 0.2;
-    if (ymax > 2.0) ymax = 2.0;
-    
-    double range = ymax - ymin;
-    h->GetYaxis()->SetRangeUser(
-        std::max(0.0, ymin - 0.15 * range),
-        ymax + 0.15 * range
-    );
-    
-    // Устанавливаем разумные границы по умолчанию если гистограмма пустая
-    if (ymin == 0.0 && ymax == 0.0) {
-        h->GetYaxis()->SetRangeUser(0.5, 1.5);
-    }
-}
-
-// void DrawAndSaveRatio(
-//     TH1D* hRatio, 
-//     LCMSAxis axis, 
-//     int centIdx, 
-//     int ktIdx,
-//     const std::string& outputDir = "."
-// ) {
-//     if (!hRatio) return;
-    
-//     // Создаём директорию если не существует
-//     gSystem->mkdir(outputDir.c_str(), kTRUE);
-    
-//     // Имя файла без расширения
-//     std::string baseName = FormatRatioName("ratio", centIdx, ktIdx, axis);
-    
-//     // Холст
-//     TCanvas* c = new TCanvas(
-//         Form("c_%s", baseName.c_str()),
-//         Form("Ratio %s cent=%d kt=%d", ToString(axis).c_str(), centIdx, ktIdx),
-//         800, 600
-//     );
-    
-//     // Поля для читаемых подписей
-//     c->SetBottomMargin(0.13);
-//     c->SetLeftMargin(0.14);
-//     c->SetRightMargin(0.05);
-//     c->SetTopMargin(0.08);
-    
-//     // Рисуем гистограмму с ошибками
-//     hRatio->Draw("P E");
-    
-//     // Горизонтальная линия на уровне 1.0
-//     TLine* line = new TLine(
-//         hRatio->GetXaxis()->GetXmin(), 1.0,
-//         hRatio->GetXaxis()->GetXmax(), 1.0
-//     );
-//     line->SetLineColor(kRed+1);
-//     line->SetLineStyle(7);  // пунктир
-//     line->SetLineWidth(2);
-//     line->Draw();
-    
-//     // Подписи на холсте
-//     TLatex latex;
-//     latex.SetNDC();
-//     latex.SetTextFont(42);
-//     latex.SetTextSize(0.045);
-    
-//     // Левый верхний угол: физические параметры
-//     latex.SetTextAlign(11);
-//     latex.DrawLatex(0.18, 0.88, ToString(axis).c_str());
-//     latex.DrawLatex(0.18, 0.82, GetCentralityLabel(centIdx).c_str());
-//     latex.DrawLatex(0.18, 0.76, GetKTRangeLabel(ktIdx).c_str());
-    
-//     // Правый верхний угол: коллаборация
-//     latex.SetTextAlign(31);
-//     latex.SetTextSize(0.042);
-//     latex.SetTextColor(kGray+2);
-//     latex.DrawLatex(0.95, 0.92, "ALICE Performance");
-    
-//     c->Update();
-    
-//     // Сохраняем в разные форматы
-//     c->SaveAs(Form("%s/%s.png", outputDir.c_str(), baseName.c_str()));
-//     c->SaveAs(Form("%s/%s.pdf", outputDir.c_str(), baseName.c_str()));
-    
-//     // Очищаем память
-//     delete c;
-// }
-
 TH1D* Project1D(TH3D& h, LCMSAxis axis, double w = 0.05) {
     SetSlice1D(h, axis, w);
 
@@ -171,86 +24,98 @@ TH1D* Project1D(TH3D& h, LCMSAxis axis, double w = 0.05) {
     return out;
 }
 
-void Style(TH1D* hRatio, LCMSAxis axis, int centIdx, int ktIdx) {
-    if (!hRatio) return;
+// h - target hist
+// fmt - formatted title string (ROOT syntax)
+void Style(
+    TH1D* h, const TString& fmt,
+    LCMSAxis axis, int centIdx, int ktIdx
+) {
+    // General
+    {
+        TString axisStr = ToString(axis);
+        TString title = TString::Format(
+            "%s at centrality=[%s] %%, k_{T}=[%s] GeV/c and %s axis",
+            fmt.Data(),
+            centralityNames[centIdx].data(), 
+            ktNames[ktIdx].data(), 
+            axisStr.Data()
+        );
 
-    TCanvas* c = new TCanvas(
-        Form(
-            "c_%s_%d_%d",
-            ToString(axis).c_str(),
-            centIdx,
-            ktIdx
-        ),
-        Form(
-            "Ratio %s cent=%d kt=%d", 
-            ToString(axis).c_str(), 
-            centIdx, 
-            ktIdx
-        ),
-        800, 600
-    );
+        h->SetTitle(title);
 
-    TPad* mainPad = new TPad("main", "main", 0.0, 0.0, 1.0, 1.0);
-    mainPad->SetBottomMargin(0.13);
-    mainPad->SetLeftMargin(0.14);
-    mainPad->SetRightMargin(0.05);
-    mainPad->SetTopMargin(0.08);
-    mainPad->Draw();
-    mainPad->cd();
-    
-    // Рисуем гистограмму
-    hRatio->Draw("P E");  // маркеры с ошибками
-    
-    // Горизонтальная линия на уровне 1.0
-    TLine* line = new TLine(hRatio->GetXaxis()->GetXmin(), 1.0,
-                           hRatio->GetXaxis()->GetXmax(), 1.0);
-    line->SetLineColor(kRed);
-    line->SetLineStyle(7);  // пунктир
-    line->SetLineWidth(2);
-    line->Draw();
+        h->SetMarkerStyle(kFullCircle);
+        h->SetMarkerSize(1.0);
+        h->SetMarkerColor(kBlack);
+        h->SetLineColor(kBlack);
+        h->SetLineWidth(2);
+    }    
 
-    TLatex latex;
-    latex.SetNDC();
-    latex.SetTextSize(0.045);
-    latex.SetTextFont(42);
-    
-    // Верхний левый угол: тип оси и диапазон kT
-    std::string ktRange = GetKTRangeLabel(ktIdx);
-    std::string centRange = GetCentralityLabel(centIdx);
-    
-    latex.DrawLatex(0.18, 0.88, Form("%s", ToString(axis).c_str()));
-    latex.DrawLatex(0.18, 0.82, Form("%s", centRange.c_str()));
-    latex.DrawLatex(0.18, 0.76, Form("%s", ktRange.c_str()));
-    
-    // Верхний правый угол: статистика (если нужно)
-    // latex.SetTextAlign(31);
-    // latex.DrawLatex(0.95, 0.88, "prelim");
-    
-    c->Update();
-    // c->SaveAs(Form("ratio_%s_cent%d_kt%d.png", ToString(axis).c_str(), centIdx, ktIdx));
-    // c->SaveAs(Form("ratio_%s_cent%d_kt%d.pdf", ToString(axis).c_str(), centIdx, ktIdx));
+    // X axis
+    {
+        TAxis* xAxis = h->GetXaxis();
+        TString axisStr = ToString(axis);
+        TString xTitle = "q_{" + axisStr + "} [GeV/c]";
+
+        xAxis->SetTitle(xTitle);
+        xAxis->CenterTitle();
+        xAxis->SetTitleSize(0.05);
+        xAxis->SetLabelSize(0.045);
+        xAxis->SetRangeUser(-0.4, 0.4);
+    }
+
+    // Y axis
+    {
+        TAxis* yAxis = h->GetYaxis();
+        yAxis->SetTitle(fmt);
+        yAxis->CenterTitle();
+        yAxis->SetTitleSize(0.05);
+        yAxis->SetLabelSize(0.045);
+        yAxis->SetTitleOffset(1.2);
+        yAxis->SetRangeUser(0.5, 1.5);
+    }
 }
 
-TH1D* ProjectRatio(TH3D& ratio3D, LCMSAxis axis, const char* name) {
-    TH1D* r = Project1D(ratio3D, axis);
-    
+TH1D* ProjectRatio(
+    TH3D& ratio,
+    int centIdx, int ktIdx,
+    LCMSAxis axis
+) {
+    TString name = TString::Format(
+        "proj_of_ratios_%d_%d_%s",
+        centIdx, ktIdx, ToString(axis).data()
+    );
+
+    TH1D* r = Project1D(ratio, axis);
     r->SetName(name);
 
+    TString axisStr = ToString(axis);
+    TString fmt = "C^{++}/C^{--}_{" + axisStr + "}";
+    
+    Style(r, fmt, axis, centIdx, ktIdx);
     return r;
 }
 
 TH1D* RatioProject(
     TH3D& Neg, TH3D& Pos,
-    LCMSAxis axis, std::string name
+    int centIdx, int ktIdx,
+    LCMSAxis axis
 ) {
+    TString name = TString::Format(
+        "ratio_proj_%d_%d_%s",
+        centIdx, ktIdx, ToString(axis).data()
+    );
+
     TH1D* n = Project1D(Neg, axis);
     TH1D* p = Project1D(Pos, axis);
 
-    auto ratio = RootPtr<TH1D>((TH1D*)n->Clone(name.c_str()));
+    TH1D* ratio = (TH1D*)n->Clone(name);
     ratio->Divide(p, n);
 
-    ApplyRatioStyle(ratio.get(), axis);
-    return ratio.release();
+    TString axisStr = ToString(axis);
+    TString fmt = "C^{++}(q_{" + axisStr + "}) / C^{--}(q_{" + axisStr + "})";
+    
+    Style(ratio, fmt, axis, centIdx, ktIdx);
+    return ratio;
 }
 
 void do_CF_ratios(TFile* fCF3D, TFile* fRatioProj, TFile* fProjRatio) {
@@ -270,18 +135,20 @@ void do_CF_ratios(TFile* fCF3D, TFile* fRatioProj, TFile* fProjRatio) {
         TH3D* ratio = (TH3D*)neg->Clone(); ratio->SetDirectory(nullptr);
         ratio->Divide(pos, neg);
 
-        for (auto ax : {LCMSAxis::Out, LCMSAxis::Side, LCMSAxis::Long}) {
-            std::string rp_name = FormatRatioName("ratio_proj", centIdx, ktIdx, ax);
-            TH1D* ratio_of_proj = RatioProject(*neg, *pos, ax, rp_name);
+        for (auto axis : {LCMSAxis::Out, LCMSAxis::Side, LCMSAxis::Long}) {
+            // Отношение проекций
+            {
+                TH1D* h = RatioProject(*neg, *pos, centIdx, ktIdx, axis);
+                fRatioProj->cd();
+                h->Write();
+            }
 
-            fRatioProj->cd();
-            ratio_of_proj->Write();
-
-            std::string proj_of_ratio_name = Form("proj_of_ratios_%d_%d_%s", centIdx, ktIdx, ToString(ax).c_str());
-            TH1D* proj_of_ratio = ProjectRatio(*ratio, ax, proj_of_ratio_name.c_str());
-
-            fProjRatio->cd();
-            proj_of_ratio->Write();
+            // Проекция отношения
+            {
+                TH1D* h = ProjectRatio(*ratio, centIdx, ktIdx, axis);
+                fProjRatio->cd();
+                h->Write();
+            }
         }
     }
 }
