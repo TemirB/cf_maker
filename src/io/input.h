7fc8f5aeaab98c66394ffefc43ae81c210bd19ca
t.balinov <balinov.tm21@physics.msu.ru>
2026-09-20 19:17:16 +0300

    dev

--- src/io/input.h
+++ src/io/input.h
@@ -8,5 +8,4 @@
 class TFile;
 
 [[nodiscard]] std::pair<TH3D*, TH3D*> get_hists(TFile* f, int ch, int centr, int bin);
-[[nodiscard]] std::string get_cf_name(int ch, int centr, const std::string& binType,
-                                    const std::string& binName);
+[[nodiscard]] std::string get_cf_name(int ch, int centr, const std::string& binType, const std::string& binName);
